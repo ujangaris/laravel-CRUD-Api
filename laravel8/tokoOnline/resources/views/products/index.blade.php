ff225c8ff28bbd317b19b50c9725845ad0794caa
ujangaris <ujangaja@gmail.com>
2021-05-26 20:55:05 +0700

    Series Tutorial Toko Online Laravel 8 + Flutter | Part 5 membaca data ditable |memanggil variable pada view

---
+++ laravel8/tokoOnline/resources/views/products/index.blade.php
@@ -0,0 +1,24 @@
+<h1>List Products</h1>
+<h3>{{ $subJudul }}</h3>
+<h2>{{ $cth }}</h2>
+<table>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Price</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Celana Boxer</td>
+            <td>Nyaman di pakai</td>
+            <td>Rp. 45.000</td>
+        </tr>
+        <tr>
+            <td>Kaos Oblong</td>
+            <td>Kualitas Import</td>
+            <td>Rp. 150.000</td>
+        </tr>
+    </tbody>
+</table>
